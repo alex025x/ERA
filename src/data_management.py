117c5ca5bea3e6b39f8ea70d8795d84d6b681de8
Alexander Lungu <lungualexbeny@gmail.com>
2024-08-19 18:22:28 +0000

    Replaced st_cache in data management file

--- src/data_management.py
+++ src/data_management.py
@@ -3,11 +3,11 @@ import pandas as pd
 import numpy as np
 import joblib
 
-@st.cache(suppress_st_warning=True, allow_output_mutation=True)
+@st.cache_data
 def load_telco_data():
     df = pd.read_csv("outputs/datasets/collection/TelcoCustomerChurn.csv")
     return df
 
-
+@st.cache_resource
 def load_pkl_file(file_path):
     return joblib.load(filename=file_path)
